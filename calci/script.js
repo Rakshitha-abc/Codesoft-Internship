714d565de7918869e06a0d145292dad441f50535
Rakshitha-abc <rakshitharashu60@gmail.com>
2024-12-20 13:01:28 +0530

    Add files via upload

---
+++ calci/script.js
@@ -0,0 +1,18 @@
+function append(value) {
+    const result = document.getElementById('result');
+    result.value += value;
+  }
+  
+  function clearResult() {
+    document.getElementById('result').value = '';
+  }
+  
+  function calculate() {
+    const result = document.getElementById('result');
+    try {
+      result.value = eval(result.value);
+    } catch (error) {
+      result.value = 'Error';
+    }
+  }
+  
